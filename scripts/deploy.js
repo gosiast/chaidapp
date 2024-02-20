68d2da3987011b5db01e7bf316b8687567b065b7
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2024-02-20 09:25:01 +0100

    removed unnecessary code from App.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,21 +4,3 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
-
-async function main() {
-	const Chai = await hre.ethers.getContractFactory("chai"); //fetching bytecode and ABI
-	//creating an object of smart contract (so we can TALK to the smart contract)
-	const chai = await Chai.deploy(); // we're not passing anything to the constructor so nothing inside the brackets
-
-	await chai.deployed(); //deploying the smart contract
-
-	console.log("Deployed contract address:", `${chai.address}`);
-}
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-	console.error(error);
-	process.exitCode = 1;
-});
