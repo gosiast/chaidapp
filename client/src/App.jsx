1936f2689654796d3a4215067d24a89dd4502c42
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2024-02-20 09:37:17 +0100

    added useEffect and started with the template code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [state, setState] = useState({
+		provider: null,
+		signer: null,
+		contract: null,
+	});
 
+	//using useeffect because wants to automatically fetch contracts
+	useEffect(() => {
+		const template = async () => {
+			const contractAddress = "";
+			const contractABI = "";
+			//here will be the code that will connect to MetaMask wallet
+			//in order to do transactions on goerli testnet
+			//metamask consists of infura api which helps in connecting to the blockchain
+		};
+	});
 	return (
 		<>
 			<div className="App"></div>
