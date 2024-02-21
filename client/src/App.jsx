dec023a289bccf1f3727aac79eeca49a9353cd3b
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2024-02-21 15:56:13 +0100

    created try catch in case user doesn't have  MetaMask account

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,26 +20,33 @@ function App() {
 			//in order to do transactions on goerli testnet
 			//metamask consists of infura api which helps in connecting to the blockchain
 			//connecting the the wallet
-			const { ethereum } = window;
-			const account = await ethereum.request({
-				method: "eth_requestAccounts", //automatically open metamask wallet whenever user visits the website
-			});
-			setAccount(account);
 
-			// provider will help us connect with the blockchain
-			//for that we need hardhat ether library
-			const provider = new ethers.providers.Web3Provider(ethereum);
+			try {
+				//try is in case someone doesn't have an account
 
-			//we have a signer - helps us doing transactions that will change state of the blockchain
-			const signer = provider.getSigner(); //it helps writing the blockchains
+				const { ethereum } = window;
+				const account = await ethereum.request({
+					method: "eth_requestAccounts", //automatically open metamask wallet whenever user visits the website
+				});
+				setAccount(account);
 
-			//3 things required to create instance
-			const contract = new ethers.Contract(
-				contractAddress, //this is where my contract is deployed, so i wanna go there
-				contractABI, // i need ABi bc i want to 'talk' to smart contract, it's required
-				signer //we can do all sort of transactions on the contract
-			);
-			setState({ provider, signer, contract });
+				// provider will help us connect with the blockchain
+				//for that we need hardhat ether library
+				const provider = new ethers.providers.Web3Provider(ethereum);
+
+				//we have a signer - helps us doing transactions that will change state of the blockchain
+				const signer = provider.getSigner(); //it helps writing the blockchains
+
+				//3 things required to create instance
+				const contract = new ethers.Contract(
+					contractAddress, //this is where my contract is deployed, so i wanna go there
+					contractABI, // i need ABi bc i want to 'talk' to smart contract, it's required
+					signer //we can do all sort of transactions on the contract
+				);
+				setState({ provider, signer, contract });
+			} catch (error) {
+				alert(error);
+			}
 		};
 
 		template();
