09fceacd02025f757804191cedca7a4340af934f
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2024-02-23 08:55:30 +0100

    updated hardhat.config to deploy smart contract on goerli testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+/** @type import('hardhat/config').HardhatUserConfig  */
+
+//we need this below to deploy smart contract on the goerli testnet
+const GOERLI_URL = process.env.GOERLI_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
-  solidity: "0.8.24",
+	solidity: "0.8.17",
+	networks: {
+		goerli: {
+			url: GOERLI_URL,
+			accounts: [PRIVATE_KEY],
+		},
+	},
 };
