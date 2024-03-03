fe727f2f35bae5b281bb5b28eee70dc725f4a46b
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2024-03-03 12:00:59 +0100

    removed hardcoded variables

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,10 +4,8 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig  */
 
 //we need this below to deploy smart contract on the goerli testnet
-const GOERLI_URL =
-	"https://eth-sepolia.g.alchemy.com/v2/z006jfB4ZHTSqK24tpUC26mI73jURGhm"; //accessing it from the .env file
-const PRIVATE_KEY =
-	"ec43e6a7e0f30693536173f9c6f29a95b17bdb9cde9c20bd8103576bd081a327"; //accessing it from the .env file
+const GOERLI_URL = process.env.GOERLI_URL; //accessing it from the .env file
+const PRIVATE_KEY = process.env.PRIVATE_KEY; //accessing it from the .env file
 
 console.log("GOERLI_URL:", GOERLI_URL);
 console.log("PRIVATE_KEY:", PRIVATE_KEY);
