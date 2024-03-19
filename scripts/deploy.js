e150ef447555a729866f865536e8abe2f74ea480
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2024-03-19 17:02:13 +0100

    string used in .env file

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,6 +11,7 @@ const hre = require("hardhat");
 
 async function main() {
 	const Chai = await hre.ethers.getContractFactory("chai"); //fetching bytecode and ABI
+	//fetching the artifacts, so we can deploy the smart contract
 	//creating an object of smart contract (so we can TALK to the smart contract)
 	const chai = await Chai.deploy(); // we're not passing anything to the constructor(in chai.sol)
 	//so nothing inside the brackets in here as well
@@ -21,6 +22,24 @@ async function main() {
 	// console.log(chai);
 
 	console.log("Deployed contract address:", `${chai.target}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+	console.error(error);
+	process.exitCode = 1;
+});
+
+const hre = require("hardhat");
+
+async function main() {
+	const Chai = await hre.ethers.getContractFactory("chai"); //fetching bytecode and ABI
+	const chai = await Chai.deploy(); //creating an instance of our smart contract
+
+	await chai.deployed(); //deploying your smart contract
+
+	console.log("Deployed contract address:", `${chai.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
