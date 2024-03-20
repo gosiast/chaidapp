6774e98681e9ded27aee967854e5a29f0dacbeef
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2024-03-20 10:17:10 +0100

    removed string from the .env file

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,10 +6,12 @@ require("dotenv").config();
 //we need this below to deploy smart contract on the goerli testnet
 const GOERLI_URL =
 	process.env.GOERLI_URL ||
-	"https://eth-sepolia.g.alchemy.com/v2/z006jfB4ZHTSqK24tpUC26mI73jURGhm"; //accessing it from the .env file
+	"https://eth-sepolia.g.alchemy.com/v2/z006jfB4ZHTSqK24tpUC26mI73jURGhm";
+//accessing it from the .env file
 const PRIVATE_KEY =
 	process.env.PRIVATE_KEY ||
-	"ec43e6a7e0f30693536173f9c6f29a95b17bdb9cde9c20bd8103576bd081a327"; //accessing it from the .env file
+	"ec43e6a7e0f30693536173f9c6f29a95b17bdb9cde9c20bd8103576bd081a327";
+//accessing it from the .env file
 
 module.exports = {
 	solidity: "0.8.17",
