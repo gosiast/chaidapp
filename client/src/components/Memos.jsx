8bc7a902c4acfd7a9ff13af58b6e4ed1438ddb7d
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2024-03-21 08:35:57 +0100

    updated the css of Memos

--- client/src/components/Memos.jsx
+++ client/src/components/Memos.jsx
@@ -11,24 +11,74 @@ const Memos = ({ state }) => {
 		const memosMessage = async () => {
 			console.log(contract);
 			const memos = await contract.getMemos();
-			console.log(memos);
+			// console.log(memos);
 		};
 		//when we have the contract then let's call memos message
 		contract && memosMessage();
 	}, [contract]);
 	return (
-		<>
-			{memos.map((memo) => {
-				return (
-					<div>
-						<p>{memo.name}</p>
-						<p>{memo.message}</p>
-						<p>{new Date(memo.timestamp * 1000).toLocaleString()}</p>
-						<p>{memo.from}</p>
-					</div>
-				);
-			})}
-		</>
+		<div className="container-fluid">
+			<h3 style={{ textAlign: "center", marginTop: "20px" }}>Messages</h3>
+			<table>
+				<tbody>
+					{memos.map((memo) => {
+						return (
+							<tr>
+								<td
+									style={{
+										backgroundColor: "dodgerblue",
+										border: "1px solid white",
+										borderCollapse: "collapse",
+										padding: "7px",
+										width: "100px",
+										color: "white",
+									}}
+								>
+									{memo.name}
+								</td>
+								<td
+									style={{
+										backgroundColor: "dodgerblue",
+										border: "1px solid white",
+										borderCollapse: "collapse",
+										padding: "7px",
+										width: "800px",
+										color: "white",
+									}}
+								>
+									{new Date(memo.timestamp * 1000).toLocaleString()}
+								</td>
+								<td
+									style={{
+										backgroundColor: "dodgerblue",
+										border: "1px solid white",
+										borderCollapse: "collapse",
+										padding: "7px",
+										width: "300px",
+										color: "white",
+									}}
+								>
+									{memo.message}
+								</td>
+								<td
+									className="container-fluid"
+									style={{
+										backgroundColor: "dodgerblue",
+										border: "1px solid white",
+										borderCollapse: "collapse",
+										padding: "7px",
+										width: "400px",
+										color: "white",
+									}}
+								>
+									{memo.from}
+								</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		</div>
 	);
 };
 export default Memos;
